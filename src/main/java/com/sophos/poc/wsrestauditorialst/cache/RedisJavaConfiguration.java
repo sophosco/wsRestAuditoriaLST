d647b120a9270a60ad43a45fde2afee9d2e0d204
jeisson.osoriod <jeisson.osoriod@SBBOGLAPPROJO7.sophos.col.com>
2019-03-31 16:34:24 -0500

    Actualizacion redis

--- src/main/java/com/sophos/poc/wsrestauditorialst/cache/RedisJavaConfiguration.java
+++ src/main/java/com/sophos/poc/wsrestauditorialst/cache/RedisJavaConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
@@ -20,7 +21,10 @@ public class RedisJavaConfiguration {
 	private static final Logger logger = LogManager.getLogger(RedisJavaConfiguration.class);
 	@Bean
 	JedisConnectionFactory jedisConnectionFactory() {
-		return new JedisConnectionFactory();
+		int 	redisPort = 6379;
+	    String  redisHost = "http://redis-master" ;	    
+		RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration(redisHost, redisPort);
+	    return new JedisConnectionFactory(redisStandaloneConfiguration);	    
 	}
 	 
 	@Bean
