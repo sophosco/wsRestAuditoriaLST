9d8634de555347d2aad4070cd7144dd45ef0cefa
jeisson.osoriod <jeisson.osoriod@SBBOGLAPPROJO7.sophos.col.com>
2019-03-31 11:57:56 -0500

    Se actualiza conexion con active mq

--- src/main/java/com/sophos/poc/wsrestauditorialst/service/ActiveMQtoActiveMQService.java
+++ src/main/java/com/sophos/poc/wsrestauditorialst/service/ActiveMQtoActiveMQService.java
@@ -18,6 +18,7 @@ import org.springframework.stereotype.Service;
 import com.sophos.poc.wsrestauditorialst.util.DefaultProperties;
 
 
+
 @Service
 public class ActiveMQtoActiveMQService {
 	
@@ -51,14 +52,6 @@ public class ActiveMQtoActiveMQService {
 
 			TextMessage producerMessage = producerSession.createTextMessage(message);
 			producer.send(producerMessage);
-			try {
-				producer.close();
-				producerSession.close();
-				producerConnection.close();
-				pooledConnectionFactory.stop();
-			} catch (Exception e) {
-				logger.error("Close Exception", e);
-			}
 			return true;
 		} catch (JMSException jmsex) {			
 			logger.error("Error Enviando Mensaje a ActiveMQ jmsex", jmsex);
@@ -76,34 +69,30 @@ public class ActiveMQtoActiveMQService {
 	}
 	
 	public boolean publishMessage(String request, String queue) {
+		Connection producerConnection = null;
+		Session producerSession = null;
+		MessageProducer producer = null;
+		PooledConnectionFactory pooledConnectionFactory = null;
 		try {
 			String message = request;
 
 			ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory(dfp.getEndpoint());
 			connectionFactory.setUserName(dfp.getUser());
 			connectionFactory.setPassword(dfp.getPass());
-			PooledConnectionFactory pooledConnectionFactory = new PooledConnectionFactory();
+			pooledConnectionFactory = new PooledConnectionFactory();
 			pooledConnectionFactory.setConnectionFactory(connectionFactory);
 			pooledConnectionFactory.setMaxConnections(10);
-			Connection producerConnection = pooledConnectionFactory.createConnection();
+			producerConnection = pooledConnectionFactory.createConnection();
 
 			producerConnection.start();
-			Session producerSession = producerConnection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+			producerSession = producerConnection.createSession(false, Session.AUTO_ACKNOWLEDGE);
 			Destination producerDestination = producerSession.createQueue(queue);
-			MessageProducer producer = producerSession.createProducer(producerDestination);
+			producer = producerSession.createProducer(producerDestination);
 			producer.setDeliveryMode(DeliveryMode.PERSISTENT);
 			producer.setTimeToLive(20000l);
 
 			TextMessage producerMessage = producerSession.createTextMessage(message);
 			producer.send(producerMessage);
-			try {
-				producer.close();
-				producerSession.close();
-				producerConnection.close();
-				pooledConnectionFactory.stop();
-			} catch (Exception e) {
-				logger.error("Close Exception", e);				
-			}
 			return true;
 		} catch (JMSException jmsex) {			
 			logger.error("Error Enviando Mensaje a ActiveMQ jmsex", jmsex);
@@ -111,6 +100,12 @@ public class ActiveMQtoActiveMQService {
 		} catch (Exception ex) {
 			logger.error("Error Enviando Mensaje a ActiveMQ ex", ex);
 			return false;
+		}finally {
+			if(producer!= null) {try {producer.close();}catch(Exception e) {logger.error("Error Closing producer: ", e);}}
+			if(pooledConnectionFactory != null) {try {pooledConnectionFactory.stop();} catch (Exception e) {logger.error("Error Stopping pooledConnectionFactory: ", e);}}		
+			if(producerSession!= null) { try {producerSession.close();} catch (Exception e) {logger.error("Error Closing  producerSession: ", e);}}
+			if(producerConnection != null) {try {producerConnection.close();} catch (Exception e) {logger.error("Error Closing producerConnection: ", e);}}				
+
 		}
 	}
 }
